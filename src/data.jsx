1e4ab6e3c7f1753e8d9eb5a1dd48ef0169228baf
Mashhur <mashhuryoldoshev2003@gmail.com>
2023-04-13 13:15:32 +0800

    newData

--- src/data.jsx
+++ src/data.jsx
@@ -5,9 +5,9 @@ const nasheeds = [
   {
     id: 1,
     artist: "Jasik",
-    category: "Mood",
-    song: "https://res.cloudinary.com/dj4lkwyjd/video/upload/v1679906165/%D2%9A%D1%83%D1%80%D0%B0%D2%93%D0%B0%D0%BD_%D0%93%D2%AF%D0%BB_Jasik_Remix_%D0%9E%D1%82_Darkhan_lt4eik.mp3",
-    img: "https://res.cloudinary.com/dj4lkwyjd/image/upload/v1680701480/3a9b3420f07435a558764e_w3tg7b.webp",
+    category: "Study",
+    song: "https://res.cloudinary.com/dj4lkwyjd/video/upload/v1681359264/study_ptvmyz.mp3",
+    img: "https://res.cloudinary.com/dj4lkwyjd/image/upload/v1681359229/study_c4e76n.png",
     name: "Darkhan",
     artistImg:
       "https://th.bing.com/th/id/R.412b676a60ed2d0901d6f0f222f10bcb?rik=%2fBgS48ZBdvFNBg&riu=http%3a%2f%2fimages.sugarscape.com%2fuserfiles%2fimage%2fSEPTEMBER2012%2fLizzieNew%2fed-sheeran-eats-cake-fan-hair-inside.jpg&ehk=6JFDub6xEtI7X8sA%2bmwMTYnKitjQXteSOV1xJJUgsbQ%3d&risl=&pid=ImgRaw&r=0",
@@ -15,9 +15,9 @@ const nasheeds = [
   {
     id: 2,
     artist: "Gafur",
-    category: "Mood",
-    song: "https://res.cloudinary.com/dj4lkwyjd/video/upload/v1679906135/%D0%AF_%D0%BB%D1%8E%D0%B1%D0%BB%D1%8E_%D1%82%D0%B5%D0%B1%D1%8F_%D0%B4%D0%B0%D0%B2%D0%BD%D0%BE_dmgfib.mp3",
-    img: "https://res.cloudinary.com/dj4lkwyjd/image/upload/v1680701480/3a9b3420f07435a558764e_w3tg7b.webp",
+    category: "Sad",
+    song: "https://res.cloudinary.com/dj4lkwyjd/video/upload/v1681359283/sad_yp7ysb.mp3",
+    img: "https://res.cloudinary.com/dj4lkwyjd/image/upload/v1681359216/sad_rsh1bv.jpg",
     name: "Darkhan",
     artistImg:
       "https://th.bing.com/th/id/R.412b676a60ed2d0901d6f0f222f10bcb?rik=%2fBgS48ZBdvFNBg&riu=http%3a%2f%2fimages.sugarscape.com%2fuserfiles%2fimage%2fSEPTEMBER2012%2fLizzieNew%2fed-sheeran-eats-cake-fan-hair-inside.jpg&ehk=6JFDub6xEtI7X8sA%2bmwMTYnKitjQXteSOV1xJJUgsbQ%3d&risl=&pid=ImgRaw&r=0",
@@ -25,9 +25,9 @@ const nasheeds = [
   {
     id: 3,
     artist: "Asilvane",
-    category: "Saad",
-    song: "https://res.cloudinary.com/dj4lkwyjd/video/upload/v1679906124/2_5199942262754123461_yjafbh.mp3",
-    img: "https://res.cloudinary.com/dj4lkwyjd/image/upload/v1679987175/188_youtube-removebg-preview_bzzby5.png",
+    category: "Relax",
+    song: "https://res.cloudinary.com/dj4lkwyjd/video/upload/v1681359321/relax_osfosj.mp3",
+    img: "https://res.cloudinary.com/dj4lkwyjd/image/upload/v1681359203/relax_mi5kp1.jpg",
     name: "Darkhan",
     artistImg:
       "https://th.bing.com/th/id/R.412b676a60ed2d0901d6f0f222f10bcb?rik=%2fBgS48ZBdvFNBg&riu=http%3a%2f%2fimages.sugarscape.com%2fuserfiles%2fimage%2fSEPTEMBER2012%2fLizzieNew%2fed-sheeran-eats-cake-fan-hair-inside.jpg&ehk=6JFDub6xEtI7X8sA%2bmwMTYnKitjQXteSOV1xJJUgsbQ%3d&risl=&pid=ImgRaw&r=0",
@@ -35,9 +35,9 @@ const nasheeds = [
   {
     id: 4,
     artist: "XXX",
-    category: "Happy",
-    song: "https://res.cloudinary.com/dj4lkwyjd/video/upload/v1679906115/2_5188592034285364873_z36sve.mp3",
-    img: "https://res.cloudinary.com/dj4lkwyjd/image/upload/v1680701480/3a9b3420f07435a558764e_w3tg7b.webp",
+    category: "Powerful",
+    song: "https://res.cloudinary.com/dj4lkwyjd/video/upload/v1681359343/motivation_fiuxyv.mp3",
+    img: "https://res.cloudinary.com/dj4lkwyjd/image/upload/v1681359175/powerfull_wz6gyd.png",
     name: "Darkhan",
     artistImg:
       "https://th.bing.com/th/id/R.412b676a60ed2d0901d6f0f222f10bcb?rik=%2fBgS48ZBdvFNBg&riu=http%3a%2f%2fimages.sugarscape.com%2fuserfiles%2fimage%2fSEPTEMBER2012%2fLizzieNew%2fed-sheeran-eats-cake-fan-hair-inside.jpg&ehk=6JFDub6xEtI7X8sA%2bmwMTYnKitjQXteSOV1xJJUgsbQ%3d&risl=&pid=ImgRaw&r=0",
